7e960be9b6367012c6f3dd34b7f20711cacf82b1
jnnnnn <jnnnnn@gmail.com>
2016-07-30 17:42:24 +1000

    Getting screenshots of the window

--- EDAP/Form1.cs
+++ EDAP/Form1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +18,31 @@ namespace EDAP
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Timer t = new Timer();
+            t.Start();
+
+            Bitmap screenshot = new Bitmap(1920, 1080);
+            try
+            {
+                IntPtr hwnd;
+
+                Process proc = Process.GetProcessesByName(Properties.Settings.Default.ProcName)[0];
+                hwnd = proc.MainWindowHandle;
+
+                screenshot = Screenshot.PrintWindow(hwnd);
+            } catch
+            {
+                Console.WriteLine("Could not find main window. Run in Administrator mode, and check settings.");
+            }
+
+            pictureBox1.Image = screenshot;
+            t.Stop();
+
+            Text = t.ToString();
+        }
+        
     }
 }
---
+++ EDAP/PrintWindow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDAP
+{
+    // from http://stackoverflow.com/a/911225/412529
+    // We need this class to get a screenshot of the ED window so we can "look" at the compass to align the next jump
+    class Screenshot
+    {
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        [DllImport("user32.dll")]
+        public static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+
+        public static Bitmap PrintWindow(IntPtr hwnd)
+        {
+            RECT rc;
+            GetWindowRect(hwnd, out rc);
+
+            Bitmap bmp = new Bitmap(rc.Width, rc.Height, PixelFormat.Format32bppArgb);
+            Graphics gfxBmp = Graphics.FromImage(bmp);
+            IntPtr hdcBitmap = gfxBmp.GetHdc();
+
+            PrintWindow(hwnd, hdcBitmap, 0);
+
+            gfxBmp.ReleaseHdc(hdcBitmap);
+            gfxBmp.Dispose();
+
+            return bmp;
+        }
+    }
+}
