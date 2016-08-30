e74b647752fe20739f3f43cbfffce7b2568d1a18
jnnnnn <jnnnnn@gmail.com>
2016-08-30 21:55:46 +1000

    Improvements to the Swoop after hitting a neutron star

--- EDAP/Pilot.cs
+++ EDAP/Pilot.cs
@@ -13,11 +13,21 @@ namespace EDAP
     /// </summary>
     class PilotJumper
     {
-        private DateTime last_jump_time = DateTime.UtcNow.AddHours(-1); // time since the "J" key was pressed        
+        private DateTime last_jump_time = DateTime.UtcNow.AddHours(-1); // time since the jump key was pressed        
         public Keyboard keyboard;
         public int jumps_remaining = 0;
-        public bool bFirstJump = false;
         private uint alignFrames = 0;
+        
+        public enum PilotState
+        {
+            None = 0,
+            firstjump = 1 << 0,
+            swoopStart = 1 << 1,            
+            swoopEnd = 1 << 2,
+            cruiseStart = 1 << 3,
+        }
+
+        public PilotState state;
 
         double SecondsSinceLastJump { get { return (DateTime.UtcNow - last_jump_time).TotalSeconds; } }
 
@@ -30,7 +40,8 @@ namespace EDAP
             {
                 last_jump_time = DateTime.UtcNow;
                 jumps_remaining = 0;
-                bFirstJump = true;
+                state = PilotState.None;
+                state |= PilotState.firstjump;
             }
             jumps_remaining += 1;
         }
@@ -38,10 +49,10 @@ namespace EDAP
         private void Jump()
         {
             keyboard.Clear();
-            keyboard.Tap(Keyboard.LetterToKey('G'));
+            keyboard.Tap(Keyboard.LetterToKey('G')); // jump            
             last_jump_time = DateTime.UtcNow;
             jumps_remaining -= 1;
-            bFirstJump = false;
+            state = PilotState.None;
         }
 
         /// <summary>
@@ -50,7 +61,8 @@ namespace EDAP
         /// <param name="compass"></param>
         public void Respond(System.Drawing.PointF compass)
         {
-            if (bFirstJump)
+            // perform the first alignment/jump immediately
+            if (state.HasFlag(PilotState.firstjump))
             {
                 if (Align(compass) && jumps_remaining > 0)
                     Jump();
@@ -60,24 +72,14 @@ namespace EDAP
             if (SecondsSinceLastJump < 30)
                 return; // charging friendship drive (15s) / countdown (5s) / witchspace (~14-16s)
 
-            if (SecondsSinceLastJump < 40)
-            {
-                // maybe in witchspace, maybe facing star
-                keyboard.Keyup(Keyboard.NumpadToKey('5'));
-                Thread.Sleep(10);
-                keyboard.Keydown(Keyboard.NumpadToKey('5')); // pitch up for ~10 seconds on arrival to avoid star.
-                Thread.Sleep(100);
-                return;
-            }
-            
             if (SecondsSinceLastJump < 45)
             {
-                // cruise away from the star for a few seconds to make it less likely that we hit it after alignment
-                keyboard.Clear();
+                Swoop();
                 return;
             }
 
-            // okay, by this point we are cruising away from the star and are ready to align and jump. We can't start charging to jump until 10 seconds after witchspace ends, but we can start aligning.
+            // okay, by this point we are cruising away from the star and are ready to align and jump. We can't start 
+            // charging to jump until 10 seconds after witchspace ends, but we can start aligning.
 
             if (Align(compass) && jumps_remaining > 0)
                 Jump();
@@ -130,6 +132,43 @@ namespace EDAP
             
             return false;
         }
+        
+        /// <summary>
+        /// At the end of a jump we are always just about to crash into the star. FFS. Pitch up for 5-15 seconds 
+        /// (depending on how long witchspace took) at 50% throttle to avoid it.
+        /// </summary>
+        private void Swoop()
+        {
+            if (SecondsSinceLastJump < 40)
+            {
+                if (!state.HasFlag(PilotState.swoopStart))
+                {
+                    keyboard.Tap(Keyboard.LetterToKey('P')); // set throttle to 50%
+                    state |= PilotState.swoopStart;
+                }
+
+                // maybe in witchspace, maybe facing star
+                keyboard.Keyup(Keyboard.NumpadToKey('5'));
+                Thread.Sleep(10);
+                keyboard.Keydown(Keyboard.NumpadToKey('5')); // pitch up for ~10 seconds on arrival to avoid star.
+                Thread.Sleep(100);
+                return;
+            }
+
+            if (SecondsSinceLastJump < 45)
+            {
+                if (!state.HasFlag(PilotState.swoopEnd))
+                {
+                    keyboard.Tap(Keyboard.LetterToKey('F')); // full throttle
+                    state |= PilotState.swoopEnd;
+                }    
+
+                // cruise away from the star for a few seconds to make it less likely that we hit it after alignment
+                keyboard.Clear();
+                return;
+            }
+        }
+
 
         /// <summary>
         /// This handles cruising behaviour. The main function is already keeping us aligned; 
@@ -141,10 +180,11 @@ namespace EDAP
         /// </summary>
         private void Cruise()
         {
-            if (SecondsSinceLastJump < 50.3 && SecondsSinceLastJump > 50)
+            if (SecondsSinceLastJump > 60 && !state.HasFlag(PilotState.cruiseStart))
             {
                 keyboard.Tap(Keyboard.LetterToKey('F')); // full throttle
                 keyboard.Tap(Keyboard.LetterToKey('Q')); // drop 25% throttle
+                state |= PilotState.cruiseStart;
             }
         }
     }
